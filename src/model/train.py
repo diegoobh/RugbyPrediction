d0fc5e5179217f3384fa03a499081c7bb02fd626
Diego Borrallo Herrero <diegobh@usal.es>
2025-04-27 18:02:51 +0200

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -1,6 +1,5 @@
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVR
 from sklearn.metrics import mean_squared_error, r2_score
